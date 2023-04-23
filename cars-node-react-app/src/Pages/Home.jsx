6f42e5790b95e19a8e2d2041d1f95085ce638181
Kristiyan Eftimov <kriskata233@abv.bg>
2023-04-23 19:55:43 +0300

    Created structure of the project and work on Home page

---
+++ cars-node-react-app/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import '../Layout/css/App.css';
+import Header from '../Layout/components/Header';
+import Footer from '../Layout/components/Footer';
+
+function Home() {
+  return (
+    <div>
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default Home;
